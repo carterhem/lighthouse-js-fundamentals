f0c838fabb2f68a46702fa5c57df4f58a0e83dcf
Carter Hemington <carter.hemington@gmail.com>
2021-12-15 14:23:46 +0000

    needed some help on this one

---
+++ looping-range.js
@@ -0,0 +1,12 @@
+function range(start, end, step) {
+  let someArray = []
+  if (start > end || step < 0 || start === undefined || end === undefined || step === undefined) {
+    return someArray;
+  } for (let x = start; x <= end; x = x + step) {
+    someArray.push(x);
+  }
+  return someArray;
+}
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
