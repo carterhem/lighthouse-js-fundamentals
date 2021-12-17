b3e831408a89a324e21f820488f280b96f05508b
Carter Hemington <carter.hemington@gmail.com>
2021-12-17 21:29:06 +0000

    looping

---
+++ concatenate-arrays.js
@@ -0,0 +1,11 @@
+function concat(array1, array2) {
+  for (var x = 0; x < array2.length; x++) {
+    array1.push(array2[x]);
+  }
+  return array1;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
